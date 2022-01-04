6953bcbd0cf4824eae4d496262684a7d3ccc798a
shauryagupta30 <41203937+shauryagupta30@users.noreply.github.com>
2022-01-04 17:53:12 +0530

    Applying request minimization strategies

--- blog/client/src/CommnetList.js
+++ blog/client/src/CommnetList.js
@@ -1,28 +1,56 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+/*import React from 'react';
 
 //Recieve {postId} as a property
 const CommentList = ({ postId }) => {
     //piece of state
-  const [comments, setComments] = useState([]);
+  //const [comments, setComments] = useState([]);
 
   //function to fetch data
-  const fetchData = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
+  // const fetchData = async () => {
+  //   const res = await axios.get(
+  //    `http://localhost:4001/posts/${postId}/comments`
+  //     //`http://localhost:4001/posts`
+  //   );
 
-    setComments(res.data);
-  };
+  //   setComments(res.data);
+  // };
   
   //call the above function form here
-  useEffect(() => {
-    fetchData();// eslint-disable-next-line
-  }, []);
+  // useEffect(() => {
+  //   fetchData();// eslint-disable-next-line
+  // }, []);
 
   //mapping the data
   const renderedComments = comments.map((comment) => {
       //returning the data
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+import React from "react";
+
+const CommentList = ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
 
